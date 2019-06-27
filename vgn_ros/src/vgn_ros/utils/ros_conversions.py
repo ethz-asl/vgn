f029a558142748e0c0d936d90cc66f2f22b7b914
Michel Breyer <michel.p.breyer@gmail.com>
2019-06-27 14:14:45 +0200

    Add more ROS conversions

--- vgn_ros/src/vgn_ros/utils/ros_conversions.py
+++ vgn_ros/src/vgn_ros/utils/ros_conversions.py
@@ -1,8 +1,57 @@
 """Common conversions between python objects and ROS messages."""
 
+import geometry_msgs.msg
 import numpy as np
 import rospy
 from sensor_msgs.msg import PointCloud2, PointField
+import std_msgs.msg
+
+
+def as_point_msg(position):
+    """Represent a numpy array as a Point message."""
+    msg = geometry_msgs.msg.Point()
+    msg.x = position[0]
+    msg.y = position[1]
+    msg.z = position[2]
+    return msg
+
+
+def as_vector3_msg(vector3):
+    """Represent a numpy array as a Vector3 message."""
+    msg = geometry_msgs.msg.Vector3()
+    msg.x = vector3[0]
+    msg.y = vector3[1]
+    msg.z = vector3[2]
+    return msg
+
+
+def as_quat_msg(orientation):
+    """Represent a `Rotation` object as a Quaternion message."""
+    quat = orientation.as_quat()
+    msg = geometry_msgs.msg.Quaternion()
+    msg.x = quat[0]
+    msg.y = quat[1]
+    msg.z = quat[2]
+    msg.w = quat[3]
+    return msg
+
+
+def as_pose_msg(transform):
+    """Represent a `Transform` object as a Pose message."""
+    msg = geometry_msgs.msg.Pose()
+    msg.position = as_point_msg(transform.translation)
+    msg.orientation = as_quat_msg(transform.rotation)
+    return msg
+
+
+def as_color_msg(color):
+    """Represent a numpy array as a ColorRGBA message."""
+    msg = std_msgs.msg.ColorRGBA()
+    msg.r = color[0]
+    msg.g = color[1]
+    msg.b = color[2]
+    msg.a = color[3] if len(color) == 4 else 1.
+    return msg
 
 
 def as_point_cloud_msg(points, colors=None, intensities=None, frame=None, stamp=None):
