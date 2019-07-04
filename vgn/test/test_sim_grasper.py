3bdcdfc3df897eb603b91109d6799f47149e7360
Michel Breyer <michel.p.breyer@gmail.com>
2019-07-04 14:43:30 +0200

    Small script to test the simulated grasper

---
+++ vgn/test/test_sim_grasper.py
@@ -0,0 +1,36 @@
+"""Small script to test the simulated grasp primitive."""
+
+import numpy as np
+
+from vgn.grasper import Grasper
+from vgn.simulation import Simulation
+from vgn.utils.transform import Rotation, Transform
+
+
+def main():
+    s = Simulation(gui=True)
+    g = Grasper(robot=s)
+
+    s.reset()
+    s.spawn_plane()
+    s.spawn_cuboid()
+    s.spawn_robot()
+    s.save_state()
+
+    rotation = np.array([[1., 0., 0.],
+                         [0., -1., 0.],
+                         [0., 0., -1.]])
+    position = np.array([0.1, 0.1, 0.01])
+    T_world_tcp = Transform(Rotation.from_dcm(rotation), position)
+
+    while True:
+        s.restore_state()
+        s.sleep(1.0)
+        result = g.grasp(T_world_tcp)
+        s.sleep(1.0)
+
+        assert result
+
+
+if __name__ == '__main__':
+    main()
