39d7e1cf9fef1a241c8882a3f33dba289090f408
Michel Breyer <michel.p.breyer@gmail.com>
2019-07-05 14:52:11 +0200

    Randomly offset grasp point

--- vgn/src/vgn/grasp_samplers.py
+++ vgn/src/vgn/grasp_samplers.py
@@ -1,6 +1,4 @@
-
 import numpy as np
-
 
 from vgn.utils.transform import Rotation, Transform
 
@@ -15,12 +13,19 @@ def uniform_grasps_on_surface(n, volume):
 
     for point, normal, axis_of_principal_curvature in zip(points, normals, curvatures):
 
+        # First, define a frame on the object surface
         R = np.empty((3, 3))
         R[:, 0] = axis_of_principal_curvature
         R[:, 1] = np.cross(axis_of_principal_curvature, normal)
         R[:, 2] = -normal
+        T_world_surface = Transform(Rotation.from_dcm(R), point)
 
-        grasp_pose = Transform(Rotation.from_dcm(R), point)
-        grasp_candidates.append(grasp_pose)
+        # Next, add a random offset along the approach vector
+        z_offset = np.random.uniform(0., 0.05)
+        T_surface_grasp = Transform(Rotation.identity(),
+                                    np.r_[0., 0., z_offset])
+
+        T_world_grasp = T_world_surface * T_surface_grasp
+        grasp_candidates.append(T_world_grasp)
 
     return grasp_candidates
