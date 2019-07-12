ca001d9b386e7d50e0ba15952284e6c6d157b452
Michel Breyer <michel.p.breyer@gmail.com>
2019-07-12 18:48:47 +0200

    Sample grasp orientations around surface normal

--- vgn/src/vgn/candidates/samplers.py
+++ vgn/src/vgn/candidates/samplers.py
@@ -1,6 +1,9 @@
+from __future__ import division
+
 import numpy as np
 import open3d
 
+from vgn.utils.misc import truncated_normal
 from vgn.utils.transform import Rotation, Transform
 
 
@@ -9,40 +12,47 @@ def uniform(point_cloud, n):
     points = np.asarray(point_cloud.points)
     normals = np.asarray(point_cloud.normals)
 
-    # Build a kd-tree for efficient lookup
-    kdtree = open3d.KDTreeFlann(point_cloud)
-    radius = 0.025
-
-    selection = np.random.choice(len(points), size=n, replace=False)
+    selection = np.random.choice(len(points), size=n)
 
     candidates = []
-    for point in points[selection]:
-        candidate = estimate_frame(normals, point, radius, kdtree)
+    for idx in selection:
+        # Add random offset to the finger tip point along the surface normal
+        z_offset = np.random.uniform(0., 0.04)
+        position = points[idx] - z_offset * normals[idx]
 
-        # Randonly shift frame along the approach vector
-        offset = Transform(Rotation.identity(),
-                           np.r_[0., 0., np.random.uniform(0., 0.05)])
+        # Sample an orientation of the grasp frame
+        orientation = sample_orientation(normals[idx])
 
-        candidates.append(candidate * offset)
+        candidates.append(Transform(orientation, position))
 
     return candidates
 
 
-def estimate_frame(normals, query, radius, kdtree):
-    """Estimate grasp frame on the surface using a PCA of the local normals.
+def sample_orientation(normal):
+    z = -normal
 
-    TODO
-        * Ensure normal is pointing in the same direction as existing ones
-    """
+    x = np.array([1., 0., 0.])
+    if np.isclose(np.abs(np.dot(x, z)), 1., 1e-4):
+        x = np.array([0., 1., 0.])
 
-    [_, nn_idx, _] = kdtree.search_radius_vector_3d(query, radius)
+    y = np.cross(z, x)
+    x = np.cross(y, z)
+    r = Rotation.from_dcm(np.vstack((x, y, z)).T)
 
-    m = np.dot(normals[nn_idx].T, normals[nn_idx])
-    w, v = np.linalg.eigh(m)
+    # Randomly perturbe around yaw, pitch and roll
+    x_rot = 0.  # truncated_normal(stddev=np.pi/18.)
+    y_rot = 0.  # truncated_normal(stddev=np.pi/18.)
+    z_rot = np.random.uniform(-np.pi/2., np.pi/2.)
+    r = r * Rotation.from_euler('xyz', [x_rot, y_rot, z_rot])
 
-    normal = v[:, np.argmax(w)]
-    curvature = v[:, np.argmin(w)]
-    binormal = np.cross(curvature, normal)
+    # TODO Flip  if y is pointing downards, to be consistent
 
-    r = np.vstack((curvature, binormal, -normal)).T
-    return Transform(Rotation.from_dcm(r), query)
+    return r
+
+
+def _truncated_normal(mean=0., stddev=1.):
+    """Values higher than 2 standard deviations are dropped and re-picked."""
+    while True:
+        sample = np.random.normal(mean, stddev)
+        if np.abs(sample) < 3 * stddev:
+            return sample
---
+++ vgn/test/test_ori_sampling.py
@@ -0,0 +1,46 @@
+"""Small script to test the simulated grasp primitive."""
+
+import numpy as np
+
+from vgn.perception import integration
+from vgn.grasper import Grasper
+from vgn.simulation import Simulation
+from vgn.utils.transform import Rotation, Transform
+from vgn.candidates.samplers import sample_orientation
+
+
+def main():
+    s = Simulation(gui=True)
+    g = Grasper(robot=s)
+
+    s.reset()
+    s.spawn_plane()
+    s.spawn_debug_cuboid()
+    s.spawn_robot()
+    s.save_state()
+
+    T_eye_world = Transform.look_at(eye=[0.1, 0.1, 0.5],
+                                    center=[0.1, 0.1, 0.],
+                                    up=[1., 0., 0.])
+
+    volume = integration.TSDFVolume(length=0.3, resolution=100)
+    rgb, depth = s.camera.get_rgb_depth(T_eye_world)
+    volume.integrate(rgb, depth, s.camera.intrinsic, T_eye_world)
+
+    point_cloud = volume.extract_point_cloud()
+
+    idx = 150
+    point = np.asarray(point_cloud.points)[idx]
+    normal = np.asarray(point_cloud.normals)[idx]
+
+    while True:
+        orientation = sample_orientation(normal)
+        T_world_tcp = Transform(orientation, point)
+
+        s.restore_state()
+        s.set_tcp_pose(T_world_tcp)
+        s.sleep(1.0)
+
+
+if __name__ == '__main__':
+    main()
