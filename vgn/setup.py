9e482df035a5e3ca02d5ca61bbe4ee5afc78e393
Michel Breyer <michel.p.breyer@gmail.com>
2019-07-18 21:01:51 +0200

    Add sub-packages to setup.py

--- vgn/setup.py
+++ vgn/setup.py
@@ -1,11 +1,16 @@
 # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD!
 
 from distutils.core import setup
+
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # Fetch values from package.xml.
 setup_args = generate_distutils_setup(
-    packages=['vgn'],
+    packages=[
+        'vgn',
+        'vgn.perception',
+        'vgn.utils',
+    ],
     package_dir={'': 'src'},
 )
 
