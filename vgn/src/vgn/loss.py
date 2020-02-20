67e7bd74f3057fe692519e7bc28404763a5bcf27
Michel Breyer <michel.p.breyer@gmail.com>
2020-02-20 15:44:52 +0100

    Fix error in rot loss function

--- vgn/src/vgn/loss.py
+++ vgn/src/vgn/loss.py
@@ -18,10 +18,11 @@ def qual_loss_fn(pred, target, mask):
 
 
 def rot_loss_fn(pred, target, mask):
-    loss0 = _quat_loss_fn(pred, target[:, 0], mask)
-    loss1 = _quat_loss_fn(pred, target[:, 1], mask)
+    loss0 = _quat_loss_fn(pred, target[:, 0])
+    loss1 = _quat_loss_fn(pred, target[:, 1])
+    loss = torch.min(loss0, loss1)
 
-    return torch.min(loss0, loss1)
+    return (loss * mask).sum() / mask.sum()
 
 
 def width_loss_fn(pred, target, mask):
@@ -29,6 +30,13 @@ def width_loss_fn(pred, target, mask):
     return (loss * mask).sum() / mask.sum()
 
 
-def _quat_loss_fn(pred, target, mask):
-    loss = 1.0 - torch.abs(torch.sum(pred * target, dim=1, keepdim=True))
-    return (loss * mask).sum() / mask.sum()
+def _quat_loss_fn(pred, target):
+    """Compute voxel-wise quat loss between pred and target volumes.
+    
+    Args:
+        pred, target: Tensors of shape Bx4x40x40x40 where dim 1 holds a quaternion.
+    
+    Returns:
+        A Bx1x40x40x40 tensor containing the voxel-wise loss.
+    """
+    return 1.0 - torch.abs(torch.sum(pred * target, dim=1, keepdim=True))
