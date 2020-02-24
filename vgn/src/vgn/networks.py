e9b6b513c0d6cfdd033e3fc43e67cc417e082b81
Michel Breyer <michel.p.breyer@gmail.com>
2020-02-24 20:22:50 +0100

    Modularize network

--- vgn/src/vgn/networks.py
+++ vgn/src/vgn/networks.py
@@ -9,76 +9,30 @@ def get_network(name):
     return models[name.lower()]
 
 
-class BaseNet(nn.Module):
-    def count_num_trainable_parameters(self):
-        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+def conv(in_channels, out_channels, kernel_size):
+    return nn.Conv3d(in_channels, out_channels, kernel_size, padding=kernel_size // 2)
 
 
-class ConvNet(BaseNet):
-    def __init__(
-        self, filters=[16, 32, 64, 64, 32, 16], kernel_sizes=[5, 3, 3, 3, 3, 5]
-    ):
+def conv_stride(in_channels, out_channels, kernel_size):
+    return nn.Conv3d(
+        in_channels, out_channels, kernel_size, stride=2, padding=kernel_size // 2
+    )
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
         super().__init__()
+        self.encoder = Encoder(1, [16, 32, 64], [5, 3, 3])
 
-        self.conv1 = nn.Conv3d(
-            1, filters[0], kernel_sizes[0], stride=2, padding=kernel_sizes[0] // 2
-        )
-        self.conv2 = nn.Conv3d(
-            filters[0],
-            filters[1],
-            kernel_sizes[1],
-            stride=2,
-            padding=kernel_sizes[1] // 2,
-        )
-        self.conv3 = nn.Conv3d(
-            filters[1],
-            filters[2],
-            kernel_sizes[2],
-            stride=2,
-            padding=kernel_sizes[2] // 2,
-        )
-        self.conv4 = nn.Conv3d(
-            filters[2], filters[3], kernel_sizes[3], padding=kernel_sizes[3] // 2
-        )
-        self.conv5 = nn.Conv3d(
-            filters[3], filters[4], kernel_sizes[4], padding=kernel_sizes[4] // 2
-        )
-        self.conv6 = nn.Conv3d(
-            filters[4], filters[5], kernel_sizes[5], padding=kernel_sizes[5] // 2
-        )
+        self.decoder = Decoder(64, [64, 32, 16], [3, 3, 5])
 
-        self.conv_qual = nn.Conv3d(filters[5], 1, kernel_size=5, padding=2)
-        self.conv_rot = nn.Conv3d(filters[5], 4, kernel_size=5, padding=2)
-        self.conv_width = nn.Conv3d(filters[5], 1, kernel_size=5, padding=2)
+        self.conv_qual = conv(16, 1, 5)
+        self.conv_rot = conv(16, 4, 5)
+        self.conv_width = conv(16, 1, 5)
 
     def forward(self, x):
-        # 1 x 40 x 40 x 40
-        x = self.conv1(x)
-        x = F.relu(x)
-
-        # 16 x 20 x 20 x 20
-        x = self.conv2(x)
-        x = F.relu(x)
-
-        # 32 x 10 x 10 x 10
-        x = self.conv3(x)
-        x = F.relu(x)
-
-        # 32 x 5 x 5 x 5
-        x = self.conv4(x)
-        x = F.relu(x)
-
-        # 32 x 5 x 5 x 5
-        x = F.interpolate(x, 10)
-        x = self.conv5(x)
-        x = F.relu(x)
-
-        # 32 x 10 x 10 x 10
-        x = F.interpolate(x, 20)
-        x = F.relu(self.conv6(x))
-
-        # 16 x 20 x 20 x 20
-        x = F.interpolate(x, 40)
+        x = self.encoder(x)
+        x = self.decoder(x)
 
         qual_out = torch.sigmoid(self.conv_qual(x))
         rot_out = F.normalize(self.conv_rot(x), dim=1)
@@ -86,3 +40,49 @@ class ConvNet(BaseNet):
 
         return qual_out, rot_out, width_out
 
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels, filters, kernels):
+        super().__init__()
+        self.conv1 = conv_stride(in_channels, filters[0], kernels[0])
+        self.conv2 = conv_stride(filters[0], filters[1], kernels[1])
+        self.conv3 = conv_stride(filters[1], filters[2], kernels[2])
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, in_channels, filters, kernels):
+        super().__init__()
+        self.conv1 = conv(in_channels, filters[0], kernels[0])
+        self.conv2 = conv(filters[0], filters[1], kernels[1])
+        self.conv3 = conv(filters[1], filters[2], kernels[2])
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = F.interpolate(x, 10)
+        x = self.conv2(x)
+        x = F.relu(x)
+
+        x = F.interpolate(x, 20)
+        x = self.conv3(x)
+        x = F.relu(x)
+
+        x = F.interpolate(x, 40)
+        return x
+
+
+def count_num_trainable_parameters(net):
+    return sum(p.numel() for p in net.parameters() if p.requires_grad)
