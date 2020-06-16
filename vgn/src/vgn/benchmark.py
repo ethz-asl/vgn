35548b9a7ca8239f70b497952d1e42502030049a
mbreyer <michel.p.breyer@gmail.com>
2020-06-16 14:37:08 +0200

    Log cloud reconstructions

--- vgn/src/vgn/benchmark.py
+++ vgn/src/vgn/benchmark.py
@@ -37,6 +37,13 @@ def main(args):
             # scan the scene
             tsdf, pc = sim.acquire_tsdf(num_viewpoints=3)
             tsdf_vol = tsdf.get_volume()
+            points = np.asarray(pc.points, dtype=np.float32)
+
+            if args.rviz:
+                vis.clear()
+                vis.workspace(sim.size)
+                vis.tsdf(tsdf_vol.squeeze(), tsdf.voxel_size)
+                vis.points(points)
 
             # plan grasps
             tic = time.time()
@@ -46,24 +53,22 @@ def main(args):
             grasps = [from_voxel_coordinates(g, tsdf.voxel_size) for g in grasps]
             toc = time.time() - tic
 
-            # visualize
-            if args.rviz:
-                vis.clear()
-                vis.workspace(sim.size)
-                vis.points(np.asarray(pc.points))
-                vis.grasps(grasps, scores, sim.config["finger_depth"])
-                vis.tsdf(tsdf_vol.squeeze(), tsdf.voxel_size)
-                vis.quality(out[0], tsdf.voxel_size)
-
             if len(grasps) == 0:
                 break  # no detections found, abort this round
 
-            # execute highest scored grasp
+            # select highest scored grasp
             grasp, score = grasps[0], scores[0]
+
+            # visualize
+            if args.rviz:
+                vis.grasps(grasps, scores, sim.config["finger_depth"])
+                vis.quality(out[0], tsdf.voxel_size)
+
+            # execute grasp
             label, _ = sim.execute_grasp(grasp.pose)
 
             # log the grasp
-            logger.log_grasp(round_id, tsdf, toc, grasp, score, label)
+            logger.log_grasp(round_id, tsdf, points, toc, grasp, score, label)
 
             if last_label == Label.FAILURE and label == Label.FAILURE:
                 consecutive_failures += 1
@@ -99,6 +104,10 @@ class Logger(object):
     def __init__(self, log_dir):
         self._root = log_dir
         self._root.mkdir(parents=True)
+        self._tsdfs_dir = self._root / "tsdfs"
+        self._tsdfs_dir.mkdir()
+        self._clouds_dir = self._root / "clouds"
+        self._clouds_dir.mkdir()
 
     def add_round(self, round_id, object_count):
         csv_path = self._root / "rounds.csv"
@@ -106,15 +115,20 @@ class Logger(object):
             io.create_csv(csv_path, "round_id,object_count")
         io.append_csv(csv_path, round_id, object_count)
 
-    def log_grasp(self, round_id, tsdf, planning_time, grasp, score, label):
+    def log_grasp(self, round_id, tsdf, points, planning_time, grasp, score, label):
         csv_path = self._root / "grasps.csv"
 
         if not csv_path.exists():
-            header = "round_id,tsdf,planning_time,i,j,k,qx,qy,qz,qw,width,score,label"
+            header = (
+                "round_id,scene_id,planning_time,i,j,k,qx,qy,qz,qw,width,score,label"
+            )
             io.create_csv(csv_path, header)
 
-        tsdf_path = self._root / (uuid.uuid4().hex + ".npz")
+        scene_id = uuid.uuid4().hex
+        tsdf_path = self._tsdfs_dir / (scene_id + ".npz")
         np.savez_compressed(str(tsdf_path), tsdf=tsdf.get_volume())
+        cloud_path = self._clouds_dir / (scene_id + ".npz")
+        np.savez_compressed(str(cloud_path), points=points)
         grasp = to_voxel_coordinates(grasp, tsdf.voxel_size)
         qx, qy, qz, qw = grasp.pose.rotation.as_quat()
         i, j, k = np.round(grasp.pose.translation).astype(np.int)
@@ -124,7 +138,7 @@ class Logger(object):
         io.append_csv(
             csv_path,
             round_id,
-            tsdf_path.name,
+            scene_id,
             planning_time,
             i,
             j,
