766e208821a8ebe5123177e332c6bbd603f59de3
Michel Breyer <10465414+mbreyer@users.noreply.github.com>
2022-03-16 19:22:11 +0100

    Add plane to gripper test script

--- test/test_sim.py
+++ test/test_sim.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 from robot_helpers.spatial import Transform, Rotation
-from vgn.simulation import GraspSim
+from vgn.simulation import GraspSim, load_urdf
 
 
 def main():
-    cfg = {"gui": True, "sleep": True, "scene": "pile"}
+    cfg = {"gui": True, "lateral_friction": 1.0}
     sim = GraspSim(cfg, np.random)
 
     ori = Rotation.from_euler("y", np.pi)
     pos = [0.0, 0.0, 0.0]
     sim.robot.reset(Transform(ori, pos), 0.08)
+
+    load_urdf("plane/model.urdf", Transform.t_[0.0, 0.0, -0.05])
 
     [sim.step() for _ in range(120)]
 
